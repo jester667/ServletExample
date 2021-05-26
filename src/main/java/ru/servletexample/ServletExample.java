b427b1c425890578e8b383c6d233bb0a7a4f1309
vladislav_tomko <vtomko@neoflex.ru>
2021-05-26 10:02:07 +0300

    git init

---
+++ src/main/java/ru/servletexample/ServletExample.java
@@ -0,0 +1,29 @@
+package ru.servletexample;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletExample extends HttpServlet {
+    private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>TestServlet</title></head>");
+        out.println("<body>");
+        out.println("<p>Hello World!</p>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
